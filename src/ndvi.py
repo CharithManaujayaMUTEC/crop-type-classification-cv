5ce7f8b4ceb603082d9ef502a5ca3ec85145f444
G.C.M. <charithmanujaya1@gmail.com>
2026-05-28 02:22:15 +0530

    ndvi

--- src/ndvi.py
+++ src/ndvi.py
@@ -1,6 +1,17 @@
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def compute_ndvi(img):
-    red = img[:, :, 2]
-    green = img[:, :, 1]
-    return (green - red) / (green + red + 1e-5)
+img = cv2.imread("test.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Fake NDVI (since no NIR band)
+red = img[:,:,0].astype(float)
+nir = img[:,:,1].astype(float)
+
+ndvi = (nir - red) / (nir + red + 1e-5)
+
+plt.imshow(ndvi, cmap='RdYlGn')
+plt.colorbar()
+plt.title("NDVI Map (Simulated)")
+plt.show()
