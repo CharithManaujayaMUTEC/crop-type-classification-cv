d8c2abe116b7c06170e704b2f0b2d87a8afe9b28
G.C.M. <charithmanujaya1@gmail.com>
2026-05-28 02:20:26 +0530

    evaluate moidifief

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,5 +1,27 @@
-def evaluate():
-    print("Evaluation placeholder")
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from tensorflow.keras.models import load_model
+from preprocessing import load_dataset
 
-if __name__ == "__main__":
-    evaluate()
+DATASET_PATH = "/content/drive/MyDrive/dataset/PlantVillage"
+
+print("Loading dataset...")
+X, y, labels = load_dataset(DATASET_PATH)
+
+y_true = np.array(y)
+
+print("Loading model...")
+model = load_model("models/crop_model.keras")
+
+print("Predicting...")
+y_pred = model.predict(X)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+cm = confusion_matrix(y_true, y_pred_classes)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+disp.plot(xticks_rotation=45)
+
+plt.title("Confusion Matrix")
+plt.show()
