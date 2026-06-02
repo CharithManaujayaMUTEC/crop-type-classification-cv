e625b242e04af7415e88eaff819c17d6f2ad4476
G.C.M. <charithmanujaya1@gmail.com>
2026-06-02 23:48:52 +0530

    evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,27 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sns
+
+from sklearn.metrics import confusion_matrix
 from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical
+
 from preprocessing import load_dataset
 
-DATASET_PATH = "/content/drive/MyDrive/dataset/PlantVillage"
+DATASET_PATH = "/content/drive/MyDrive/dataset/EuroSAT_RGB/EuroSAT_RGB"
 
 print("Loading dataset...")
 X, y, labels = load_dataset(DATASET_PATH)
 
-y_true = np.array(y)
+y_cat = to_categorical(y, num_classes=len(labels))
 
 print("Loading model...")
-model = load_model("models/crop_model.keras")
+model = load_model("models/eurosat_model.keras")
 
 print("Predicting...")
-y_pred = model.predict(X)
-y_pred_classes = np.argmax(y_pred, axis=1)
+y_pred = model.predict(X, verbose=1)
 
-cm = confusion_matrix(y_true, y_pred_classes)
+y_true = np.argmax(y_cat, axis=1)
+y_pred = np.argmax(y_pred, axis=1)
 
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
-disp.plot(xticks_rotation=45)
+cm = confusion_matrix(y_true, y_pred)
 
-plt.title("Confusion Matrix")
+plt.figure(figsize=(10, 8))
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt="d",
+    cmap="Blues",
+    xticklabels=labels,
+    yticklabels=labels
+)
+
+plt.title("EuroSAT Confusion Matrix")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+
+plt.tight_layout()
+plt.savefig("results/confusion_matrix.png")
 plt.show()
+
+print("Saved: results/confusion_matrix.png")
