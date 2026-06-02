ed9a4b4601771ce328bb96beecb4a790c750d0f9
G.C.M. <charithmanujaya1@gmail.com>
2026-06-02 23:40:57 +0530

    predict py

--- src/predict.py
+++ src/predict.py
@@ -2,19 +2,23 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model("models/crop_model.keras")
+MODEL_PATH = "models/eurosat_model.keras"
+LABELS_PATH = "models/eurosat_labels.npy"
 
-labels = [
-    "Pepper_healthy", "Potato_healthy", "Potato_Early_blight",
-    "Potato_Late_blight", "Tomato_Early_blight"
-]
+model = load_model(MODEL_PATH)
+labels = np.load(LABELS_PATH, allow_pickle=True)
 
-img = cv2.imread("test.jpg")
-img = cv2.resize(img, (128,128))
-img = img / 255.0
+IMAGE_PATH = "/content/drive/MyDrive/dataset/EuroSAT_RGB/EuroSAT_RGB/Forest/Forest_1.jpg"
+
+img = cv2.imread(IMAGE_PATH)
+img = cv2.resize(img, (64, 64))
+img = img.astype("float32") / 255.0
+
 img = np.expand_dims(img, axis=0)
 
-pred = model.predict(img)
-class_id = np.argmax(pred)
+pred = model.predict(img, verbose=0)[0]
 
-print("Predicted class:", labels[class_id])
+class_idx = np.argmax(pred)
+
+print("Prediction:", labels[class_idx])
+print("Confidence:", round(pred[class_idx] * 100, 2), "%")
